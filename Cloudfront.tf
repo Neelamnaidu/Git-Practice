c3cab26857a5b3c298da84d5122f3f385729ae33
Neelamnaidu <118432826+Neelamnaidu@users.noreply.github.com>
2024-11-08 01:34:01 +0530

    Update Cloudfront.tf

--- Cloudfront.tf
+++ Cloudfront.tf
@@ -3,5 +3,5 @@
 #   s3_origin_id      = module.dev_s3_1.bucket_id
 #   s3_domain_name    = "edo-frontend-bucket.s3-website-us-east-1.amazonaws.com"
 #   distribution_name = "edo-frontend-distribution"
-#   region            = "us-east-1"
+   region            = "us-east-1"
 }
