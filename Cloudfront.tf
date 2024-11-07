05e2e745d69f0fa5714bdcc072884f552960f771
Neelamnaidu <neelamnaidu183@gmail.com>
2024-11-08 00:41:48 +0530

    Git Practice Code

---
+++ Cloudfront.tf
@@ -0,0 +1,7 @@
+# module "dev_Cloudfront_1" {
+#   source            = "../modules/Cloudfront"
+#   s3_origin_id      = module.dev_s3_1.bucket_id
+#   s3_domain_name    = "edo-frontend-bucket.s3-website-us-east-1.amazonaws.com"
+#   distribution_name = "edo-frontend-distribution"
+#   region            = "us-east-1"
+# }
